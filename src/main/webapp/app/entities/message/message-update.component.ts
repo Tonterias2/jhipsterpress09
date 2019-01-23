79a2d55a113169425f048f40588cec16de733444
Tonterias <ecorreos@htomail.com>
2019-01-23 18:29:35 +0100

    Bloscked user messages fix commit

--- src/main/webapp/app/entities/message/message-update.component.ts
+++ src/main/webapp/app/entities/message/message-update.component.ts
@@ -39,12 +39,12 @@ export class MessageUpdateComponent implements OnInit {
     blockusers: IBlockuser[];
 
     currentAccount: any;
-    isBlocked: boolean;
+    isAllowedUser: boolean;
 
     routeData: any;
     nameParamFollows: any;
     valueParamFollows: number;
-    blockedByUser: string;
+    blockeduserId: number;
 
     alerts: any[];
 
@@ -68,11 +68,9 @@ export class MessageUpdateComponent implements OnInit {
     }
 
     ngOnInit() {
-        //        setInterval(() => {
-        //            this.onWarning('BLOCKED BY USER');
-        //            }, 5000);
-        this.onWarning('BLOCKED BY USER');
+        //        this.onWarning('BLOCKED BY USER');
         this.isSaving = false;
+        this.isAllowedUser = true;
         this.activatedRoute.data.subscribe(({ message }) => {
             this.message = message;
             this.creationDate = moment().format(DATE_TIME_FORMAT);
@@ -84,30 +82,29 @@ export class MessageUpdateComponent implements OnInit {
             this.uprofileService.query(query).subscribe(
                 (res: HttpResponse<IUprofile[]>) => {
                     this.message.receiverId = res.body[0].userId;
+                    this.blockeduserId = res.body[0].userId;
                     console.log('CONSOLOG: M:ngOnInit & O: this.message.receiverId:', this.message.receiverId);
+                    this.principal.identity().then(account => {
+                        this.currentAccount = account;
+                        this.message.senderId = this.currentAccount.id;
+                        console.log('CONSOLOG: M:ngOnInit & O: this.currentAccount : ', this.currentAccount);
+                        this.isBlockUser().subscribe(
+                            (res2: HttpResponse<IBlockuser[]>) => {
+                                this.blockusers = res2.body;
+                                console.log('CONSOLOG: M:currentLoggedProfile & O:  this.blockusers : ', this.blockusers);
+                                if (this.blockusers.length > 0) {
+                                    this.isAllowedUser = false;
+                                    this.valueParamFollows = null;
+                                    //                                    this.onWarning('BLOCKED BY USER');
+                                    console.log('CONSOLOG: M:currentLoggedProfile & O:  this.isAllowedUser : ', this.isAllowedUser);
+                                    return this.blockusers[0];
+                                }
+                            },
+                            (res3: HttpErrorResponse) => this.onError(res3.message)
+                        );
+                    });
                 },
                 (res: HttpErrorResponse) => this.onError(res.message)
-            );
-        });
-        this.principal.identity().then(account => {
-            this.currentAccount = account;
-            this.message.senderId = this.currentAccount.id;
-            console.log('CONSOLOG: M:ngOnInit & O: this.currentAccount : ', this.currentAccount);
-            //            this.myMessagesCommunities();
-            //            this.currentLoggedUser();
-            this.isBlockUser().subscribe(
-                (res: HttpResponse<IBlockuser[]>) => {
-                    this.blockusers = res.body;
-                    console.log('CONSOLOG: M:currentLoggedProfile & O:  this.blockusers : ', this.blockusers);
-                    if (this.blockusers.length > 0) {
-                        this.isBlocked = true;
-                        this.valueParamFollows = null;
-                        this.onWarning('BLOCKED BY USER');
-                        console.log('CONSOLOG: M:currentLoggedProfile & O:  this.isBlocked : ', this.isBlocked);
-                        return this.blockusers[0];
-                    }
-                },
-                (res3: HttpErrorResponse) => this.onError(res3.message)
             );
         });
     }
@@ -123,32 +120,19 @@ export class MessageUpdateComponent implements OnInit {
             this.subscribeToSaveResponse(this.messageService.update(this.message));
         } else {
             if (this.message.receiverId !== undefined) {
-                if (this.isBlocked === false) {
+                if (this.isAllowedUser === true) {
                     console.log('CONSOLOG: M:save & O: this.isBlockUser.length : NO-BLOCKED ', this.isBlockUser.length);
                     this.subscribeToSaveResponse(this.messageService.create(this.message));
-                    //                    } else {
-                    ////                        this.valueParamFollows = null;
-                    ////                        this.jhiAlertService.error('BLOCKED BY USER', {type: 'warning', msg: 'BLOCKED BY USER'});
-                    ////                        this.jhiAlertService.addAlert({type: 'warning', msg: 'BLOCKED BY USER', timeout: 10000}, []);
-                    ////                        this.jhiAlertService.error(msg: 'BLOCKED BY USER');
-                    //                        this.blockedByUser = 'BLOCKED BY USER';
-                    //                        console.log('CONSOLOG: M:save & O: this.blockedByUser : ', this.blockedByUser);
-                    ////                        this.onBlockedUserError(this.blockedByUser);
                 }
             }
-            //            this.subscribeToSaveResponse(this.messageService.create(this.message));
         }
     }
 
     private isBlockUser() {
-        this.isBlocked = false;
+        this.isAllowedUser = true;
         const query = {};
         if (this.currentAccount.id != null) {
-            //            query['blockeduserId.in'] = this.loggedUser.id;
-            //            query['blockinguserId.in'] = Number(this.valueParamFollows);
-            //            query['blockeduserId.in'] = this.currentAccount.id;
-            //            query['blockinguserId.in'] = this.message.receiverId;
-            query['blockeduserId.in'] = Number(this.valueParamFollows);
+            query['blockeduserId.in'] = this.blockeduserId;
             query['blockinguserId.in'] = this.currentAccount.id;
         }
         console.log('CONSOLOG: M:isBlockUser & O: query : ', query);
@@ -172,37 +156,23 @@ export class MessageUpdateComponent implements OnInit {
         this.jhiAlertService.error(errorMessage, null, null);
     }
 
-    private onWarning(errorMessage: string) {
-        console.log('CONSOLOG: M:onWarning & O:  errorMessage : ', errorMessage);
-        //        this.jhiAlertService.addAlert({type: 'warning', msg: errorMessage, timeout: 10000}, null, null);
-        //                this.jhiAlertService.addAlert({type: 'warning', msg: errorMessage, timeout: 10000}, []);
-        //        this.jhiAlertService.warning('TEST', {type: 'warning', msg: errorMessage});
-        //        this.jhiAlertService.warning(errorMessage, null, null);
-        //        this.jhiAlertService.addAlert(this.jhiAlertService.warning(errorMessage, null, null));
-        // estas no dan errores
-        //        this.jhiAlertService.addAlert({type: 'warning', msg: errorMessage, timeout: 5000}, []);
-        //        this.jhiAlertService.addAlert({type: 'warning', msg: errorMessage, timeout: 5000}, null);
-        this.alerts = [];
-        //        this.jhiAlertService.warning(errorMessage);
-        console.log('CONSOLOG: M:onWarning & O:  this.alerts : ', this.alerts);
-        this.alerts.push(
-            this.jhiAlertService.addAlert(
-                {
-                    type: 'info',
-                    msg: errorMessage,
-                    timeout: 5000,
-                    toast: false,
-                    scoped: true
-                },
-                this.alerts
-            )
-        );
-        console.log('CONSOLOG: M:onWarning & O:  this.alerts2 : ', this.alerts);
-    }
-    //
-    //    private onBlockedUserError(blockedByUser: string) {
-    //        console.log('CONSOLOG: M:onBlockedUserError & O: this.blockedByUser : ', blockedByUser);
-    //        this.jhiAlertService.info(blockedByUser, null, null);
+    //    private onWarning(errorMessage: string) {
+    //        console.log('CONSOLOG: M:onWarning & O:  errorMessage : ', errorMessage);
+    //        this.alerts = [];
+    //        console.log('CONSOLOG: M:onWarning & O:  this.alerts : ', this.alerts);
+    //        this.alerts.push(
+    //            this.jhiAlertService.addAlert(
+    //                {
+    //                    type: 'info',
+    //                    msg: errorMessage,
+    //                    timeout: 5000,
+    //                    toast: false,
+    //                    scoped: true
+    //                },
+    //                this.alerts
+    //            )
+    //        );
+    //        console.log('CONSOLOG: M:onWarning & O:  this.alerts2 : ', this.alerts);
     //    }
 
     trackUserById(index: number, item: IUser) {
